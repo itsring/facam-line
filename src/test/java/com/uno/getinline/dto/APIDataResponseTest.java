5365853ed970427c0afa3e7f0212fd9998110a87
psjCom <psj@bestco.kr>
2022-06-15 16:58:26 +0900

    tdd 실습
    1234

--- src/test/java/com/uno/getinline/dto/APIDataResponseTest.java
+++ src/test/java/com/uno/getinline/dto/APIDataResponseTest.java
@@ -8,37 +8,47 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class APIDataResponseTest {
 
-    @DisplayName("문자열 데이터가 주어지면, 표준 성공 응답을 생성한다.")
+    @DisplayName("문자열 데이터가 주어지면 , 표준 성공 응답을 생성한다.")
+   @Test
+    void givenStringData_whenCreatingResponse_thenReturnsSuccessfulResponse(){
+
+       //Given
+       String data ="test data";
+
+       //When
+       APIDataResponse<String> response = APIDataResponse.of(data);
+
+       //Then
+       assertThat(response)
+               .hasFieldOrPropertyWithValue("success", true)
+               .hasFieldOrPropertyWithValue("errorCode", ErrorCode.OK.getCode())
+               .hasFieldOrPropertyWithValue("message", ErrorCode.OK.getMessage())
+               .hasFieldOrPropertyWithValue("data",data);
+
+
+
+   }
+
+    @DisplayName("데이터가 없으면 ,비어있는 표준 성공 응답을 생성한다.")
     @Test
-    void givenStringData_whenCreatingResponse_thenReturnsSuccessfulResponse() {
-        // Given
-        String data = "test data";
+    void givenStringData_whenCreatingResponse_thenReturnsEmptySuccessfulResponse(){
 
-        // When
-        APIDataResponse<String> response = APIDataResponse.of(data);
+        //Given
 
-        // Then
-        assertThat(response)
-                .hasFieldOrPropertyWithValue("success", true)
-                .hasFieldOrPropertyWithValue("errorCode", ErrorCode.OK.getCode())
-                .hasFieldOrPropertyWithValue("message", ErrorCode.OK.getMessage())
-                .hasFieldOrPropertyWithValue("data", data);
-    }
 
-    @DisplayName("데이터가 없을 때, 비어있는 표준 성공 응답을 생성한다.")
-    @Test
-    void givenNothing_whenCreatingResponse_thenReturnsEmptySuccessfulResponse() {
-        // Given
-
-        // When
+        //When
         APIDataResponse<String> response = APIDataResponse.empty();
 
-        // Then
+        //Then
         assertThat(response)
                 .hasFieldOrPropertyWithValue("success", true)
                 .hasFieldOrPropertyWithValue("errorCode", ErrorCode.OK.getCode())
                 .hasFieldOrPropertyWithValue("message", ErrorCode.OK.getMessage())
-                .hasFieldOrPropertyWithValue("data", null);
+                .hasFieldOrPropertyWithValue("data",null);
+
+
+
     }
+
 
 }
